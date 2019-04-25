10db25fe9e9965aafa7ef0a9a55134e9f99f1849
Unknown <cheng.zhang@polytechnique.edu>
2019-04-25 11:23:59 +0200

    add euclidean and hyperbolic embedding

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 # import matplotlib.pyplot as plt
 import tensorflow as tf
-
+import networkx as nx
 
 def cdist(X, Y):
     X2 = tf.reduce_sum(tf.square(X), 1)
@@ -63,14 +63,38 @@ def wasserstein_distance(n1, n2, embeddings, u, v, lambd, p, n_iter, tol):
     return distance
 
 
-def embedding_distances(pairs, embeddings, u, v, lambd, p, n_iter, tol):
-
+def wasserstein_distances(pairs, embeddings, u, v, lambd, p, n_iter, tol):
     results = tf.map_fn(lambda x: wasserstein_distance(
-        x[0], x[1], embeddings, u, v, lambd, p, n_iter, tol), (pairs), dtype=tf.float64)
+        x[0], x[1], embeddings, u, v, lambd, p, n_iter, tol), pairs, dtype=tf.float64)
+    return results
+
+def euclidean_distance(n1, n2, embeddings):
+    v1 = embeddings[n1, :]
+    v2 = embeddings[n2, :]
+    distance = tf.sqrt(tf.reduce_sum(tf.square(v1 - v2)))
+    return distance
+
+def euclidean_distances(pairs, embeddings):
+    results = tf.map_fn(lambda x: euclidean_distance(x[0], x[1], embeddings), pairs, dtype=tf.float64)
+    return results
+
+def hyperbolic_distance(n1, n2, embeddings, eps):
+    v1 = embeddings[n1, :]
+    v2 = embeddings[n2, :]
+    norm1 = tf.norm(v1)
+    norm2 = tf.norm(v2)
+    v1 = tf.cond(tf.greater_equal(norm1, 1), lambda: v1 / norm1 - eps, lambda: v1)
+    v2 = tf.cond(tf.greater_equal(norm2, 1), lambda: v2 / norm2 - eps, lambda: v2)
+    
+    distance = tf.acosh(1 + 2 * tf.reduce_sum(tf.square(v1 - v2)) / ((1 - tf.reduce_sum(tf.square(v1))) * (1 - tf.reduce_sum(tf.square(v2)))))
+    return distance
+
+def hyperbolic_distances(pairs, embeddings, eps):
+    results = tf.map_fn(lambda x: hyperbolic_distance(x[0], x[1], embeddings, eps), pairs, dtype=tf.float64)
     return results
 
 
-def train(node_pairs, obj_distances, n_epochs=500, patience=10, learning_rate=0.5, u_v=None, nodes=128, embed_dim=20, ground_dim=2, lambd=1.0, p=1, mat_bal_iter=20, mat_bal_tol=1e-5):
+def train(node_pairs, obj_distances, embedding_type='Euc', n_epochs=500, patience=10, learning_rate=0.01, u_v=None, nodes=128, embed_dim=20, ground_dim=2, lambd=1.0, p=1, mat_bal_iter=20, mat_bal_tol=1e-5, eps=1e-5):
     if u_v is None:
         u = tf.ones([embed_dim, 1], dtype=tf.float64) / embed_dim
         v = tf.ones([embed_dim, 1], dtype=tf.float64) / embed_dim
@@ -82,9 +106,17 @@ def train(node_pairs, obj_distances, n_epochs=500, patience=10, learning_rate=0.
     Lambd = tf.placeholder(dtype=tf.float64, shape=(), name='Lambd')
     Learning_rate = tf.placeholder(dtype=tf.float64, shape=(), name='Learning_rate')
 
-    Embeddings = tf.Variable(tf.random.normal(
+    if (embedding_type == 'Wass'):
+        Embeddings = tf.Variable(tf.random.uniform(
         [nodes, embed_dim, ground_dim], dtype=tf.float64), name='Embeddings')
-    Embed_Distances = embedding_distances(Node_Pairs, Embeddings, u, v, Lambd, p, mat_bal_iter, mat_bal_tol)
+        Embed_Distances = wasserstein_distances(Node_Pairs, Embeddings, u, v, Lambd, p, mat_bal_iter, mat_bal_tol)
+    elif (embedding_type == 'Hyper'):
+        Embeddings = tf.Variable(0.002 * tf.random.uniform([nodes, embed_dim], dtype=tf.float64) - 0.001, name='Embeddings')
+        Embed_Distances = hyperbolic_distances(Node_Pairs, Embeddings, eps)
+    else:
+        Embeddings = tf.Variable(tf.random.uniform([nodes, embed_dim], dtype=tf.float64), name='Embeddings')
+        Embed_Distances = euclidean_distances(Node_Pairs, Embeddings)
+
     Loss = tf.reduce_mean(tf.abs(Embed_Distances - Obj_Distances) / Obj_Distances)
     Jac = tf.gradients(ys=Embed_Distances, xs=Embeddings)
     optimizer = tf.train.AdamOptimizer(Learning_rate).minimize(Loss)
@@ -116,13 +148,15 @@ def train(node_pairs, obj_distances, n_epochs=500, patience=10, learning_rate=0.
 
 from graph_generator import GraphGenerator
 
-g = GraphGenerator(graph_type='scale-free', n_nodes=128, m=3)
+# g = GraphGenerator(graph_type='scale-free', n_nodes=128, m=3)
+g = nx.read_gpickle("./graphs/scale_free_1.pickle")
 node_pairs = g.get_node_pairs()
 print(node_pairs.shape)
 obj_distances = g.get_obj_distances()
 print(obj_distances.shape)
 
-embeddings, loss_history, embed_distances, jac = train(node_pairs, obj_distances)
+# learning rate = 0.1 for Euc and Wass, 0.01 for Hyper
+embeddings, loss_history, embed_distances, jac = train(node_pairs, obj_distances, embedding_type='Hyper')
 
 # plt.figure()
 # plt.plot(loss_history)
